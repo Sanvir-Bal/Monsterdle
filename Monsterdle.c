d16b79637e0839edf784d14f415c7bab88ff2c9a
Sanvir-Bal <83791711+Sanvir-Bal@users.noreply.github.com>
2022-03-24 16:30:18 -0700

    Update Monsterdle.c

--- Monsterdle.c
+++ Monsterdle.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <pthread.h>
 #include <unistd.h>
+#include<time.h>
 //Definitons
 #define NODE struct creature
 //Customer Creature
@@ -13,6 +14,7 @@ struct creature{
     int challengeRating;
     char type[50];
     char size[20];
+    int sizeNumber;
     int armorClass;
     int hitPoints;
     char alignment[50];
@@ -20,114 +22,118 @@ struct creature{
 
 //Function Prototypes
 void readFile(void);
+void checkGuess(char *guess);
+int checkSize(int i);
 
 NODE monsterdle[295];
+NODE answer;
 
 int main()
 {
     readFile();
-    for(int i = 0; i < 295; i++){
-        printf("Name: %s\n", monsterdle[i].name);
-        printf("CR: %d\n", monsterdle[i].challengeRating);
-        printf("Type: %s\n", monsterdle[i].type);
-        printf("Size: %s\n", monsterdle[i].size);
-        printf("AC: %d\n", monsterdle[i].armorClass);
-        printf("HP: %d\n", monsterdle[i].hitPoints);
-        printf("Alignment: %s\n", monsterdle[i].alignment);
-    }
+    time_t t;
+    srand((unsigned) time(&t));
     int monster = rand() % 295;
-    printf("%d\n", monster);
-    NODE answer = monsterdle[120];
-    printf("Name: %s\n", answer.name);
-    printf("CR: %d\n", answer.challengeRating);
-    printf("Type: %s\n", answer.type);
-    printf("Size: %s\n", answer.size);
-    printf("AC: %d\n", answer.armorClass);
-    printf("HP: %d\n", answer.hitPoints);
-    printf("Alignment: %s\n", answer.alignment);
+    answer = monsterdle[monster];
     int counter = 0;
-    NODE guess;
-    int guessIndex;
+    char guess[100];
     while(counter < 6){
         printf("What is your guess?\n");
-        scanf("%s", guess.name);
-        if(strcmp(guess.name, answer.name) == 0){
+        scanf("%s", guess);
+        if(strcmp(guess, answer.name) == 0){
             printf("You Win!\n");
             counter = 7;
             break;
         }
         else{
             counter++;
-            for(int i = 0; i < 295; i++){
-                if(strcmp(guess.name, monsterdle[i].name) == 0){
-                    guessIndex = i;
-                    //i = 295;
-                }
-            }
-            printf("Name: %s\n", monsterdle[guessIndex].name);
-            if(strcmp(monsterdle[guessIndex].name, answer.name) == 0){
-                printf("Correct\n");
-            }
-            else{
-                printf("Incorrect\n");
-            }
-            printf("CR: %d\n", monsterdle[guessIndex].challengeRating);
-            if(monsterdle[guessIndex].challengeRating == answer.challengeRating){
-                printf("Correct\n");
-            }
-            else if(monsterdle[guessIndex].challengeRating >= answer.challengeRating){
-                printf("Lower\n");
-            }
-            else{
-                printf("Higher\n");
-            }
-            printf("Type: %s\n", monsterdle[guessIndex].type);
-            if(strcmp(monsterdle[guessIndex].type, answer.type) == 0){
-                printf("Correct\n");
-            }
-            else{
-                printf("Incorrect\n");
-            }
-            printf("Size: %s\n", monsterdle[guessIndex].size);
-            if(strcmp(monsterdle[guessIndex].size, answer.size) == 0){
-                printf("Correct\n");
-            }
-            else{
-                printf("Incorrect\n");
-            }
-            printf("AC: %d\n", monsterdle[guessIndex].armorClass);
-            if(monsterdle[guessIndex].armorClass == answer.armorClass){
-                printf("Correct\n");
-            }
-            else if(monsterdle[guessIndex].armorClass >= answer.armorClass){
-                printf("Lower\n");
-            }
-            else{
-                printf("Higher\n");
-            }
-            printf("HP: %d\n", monsterdle[guessIndex].hitPoints);
-            if(monsterdle[guessIndex].hitPoints == answer.hitPoints){
-                printf("Correct\n");
-            }
-            else if(monsterdle[guessIndex].hitPoints >= answer.hitPoints){
-                printf("Lower\n");
-            }
-            else{
-                printf("Higher\n");
-            }
-            printf("Alignment: %s\n", monsterdle[guessIndex].alignment);
-            if(strcmp(monsterdle[guessIndex].alignment, answer.alignment) == 0){
-                printf("Correct\n");
-            }
-            else{
-                printf("Incorrect\n");
-            }
+            checkGuess(guess);
         }
     }
     if(counter == 6){
         printf("You Lose!\n");
+        printf("The Answer Was: %s\n", answer.name);
+        printf("CR: %d\n", answer.challengeRating);
+        printf("Type: %s\n", answer.type);
+        printf("Size: %s\n", answer.size);
+        printf("AC: %d\n", answer.armorClass);
+        printf("HP: %d\n", answer.hitPoints);
+        printf("Alignment: %s\n", answer.alignment);
+        
     }
 }
+
+void checkGuess(char *guess){
+    int guessIndex;
+    for(int i = 0; i < 295; i++){
+        if(strcmp(guess, monsterdle[i].name) == 0){
+            guessIndex = i;
+            i = 295;
+        }
+    }
+    printf("Name: %s\n", monsterdle[guessIndex].name);
+    if(strcmp(monsterdle[guessIndex].name, answer.name) == 0){
+        printf("Correct\n");
+    }
+    else{
+        printf("Incorrect\n");
+    }
+    printf("CR: %d\n", monsterdle[guessIndex].challengeRating);
+    if(monsterdle[guessIndex].challengeRating == answer.challengeRating){
+        printf("Correct\n");
+    }
+    else if(monsterdle[guessIndex].challengeRating >= answer.challengeRating){
+        printf("Lower\n");
+    }
+    else{
+        printf("Higher\n");
+    }
+    printf("Type: %s\n", monsterdle[guessIndex].type);
+    if(strcmp(monsterdle[guessIndex].type, answer.type) == 0){
+        printf("Correct\n");
+    }
+    else{
+        printf("Incorrect\n");
+    }
+    printf("Size: %s\n", monsterdle[guessIndex].size);
+    if(monsterdle[guessIndex].sizeNumber == answer.sizeNumber){
+        printf("Correct\n");
+    }
+    else if(monsterdle[guessIndex].sizeNumber >= answer.sizeNumber){
+        printf("Lower\n");
+    }
+    else{
+        printf("Higher\n");
+    }
+    printf("AC: %d\n", monsterdle[guessIndex].armorClass);
+    if(monsterdle[guessIndex].armorClass == answer.armorClass){
+        printf("Correct\n");
+    }
+    else if(monsterdle[guessIndex].armorClass >= answer.armorClass){
+        printf("Lower\n");
+    }
+    else{
+        printf("Higher\n");
+    }
+    printf("HP: %d\n", monsterdle[guessIndex].hitPoints);
+    if(monsterdle[guessIndex].hitPoints == answer.hitPoints){
+        printf("Correct\n");
+    }
+    else if(monsterdle[guessIndex].hitPoints >= answer.hitPoints){
+        printf("Lower\n");
+    }
+    else{
+        printf("Higher\n");
+    }
+    printf("Alignment: %s\n", monsterdle[guessIndex].alignment);
+    if(strcmp(monsterdle[guessIndex].alignment, answer.alignment) == 0){
+        printf("Correct\n");
+    }
+    else{
+        printf("Incorrect\n");
+    }
+}
+
 void readFile(){
     int flag = 1;
     char tempName[50];
@@ -148,6 +154,7 @@ void readFile(){
                 break;
             case 4:
                 strcpy(monsterdle[i].size, tempName);
+                monsterdle[i].sizeNumber = checkSize(i);
                 break;
             case 5:
                 monsterdle[i].armorClass = atoi(tempName);
@@ -167,4 +174,25 @@ void readFile(){
         }
     }
     fclose(fp);
+}
+
+int checkSize(int i){
+        if(strcmp(monsterdle[i].size, "Tiny") == 0){
+            return 1;
+        }
+        else if(strcmp(monsterdle[i].size, "Small") == 0){
+            return 2;
+        }
+        else if(strcmp(monsterdle[i].size, "Medium") == 0){
+            return 3;
+        }
+        else if(strcmp(monsterdle[i].size, "Large") == 0){
+            return 4;
+        }
+        else if(strcmp(monsterdle[i].size, "Huge") == 0){
+            return 5;
+        }
+        else if(strcmp(monsterdle[i].size, "Gargantuan") == 0){
+            return 6;
+        }
 }
